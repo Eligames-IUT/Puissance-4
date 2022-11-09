5682ab9785b7373e32ac8cd650eeb3f34a9a98ca
Julien Charbonnel <juliencharbonnel20@gmail.com>
2022-11-09 09:24:43 +0100

    avancement ptojet Création de la classe écoutant les clics

--- app/src/main/java/fr/groupe1C/puissance4/Joueur.java
+++ app/src/main/java/fr/groupe1C/puissance4/Joueur.java
@@ -1,5 +1,14 @@
+package fr.groupe1C.puissance4;
+
 import java.awt.Color;
 
+/**
+ * 
+ * @author Julien Charbonnel
+ * @version 1.0
+ * Classe qui représente un joueur
+ * 
+ */
 public class Joueur {
     
     private int id;
@@ -56,6 +65,45 @@ public class Joueur {
             algorithme.jouerColonne(colonne);
         }
         
+    }
+
+   /**
+   * méthode qui permet au joueur de jouer un tour
+   * @param colonne
+   */
+   public static void jouerColonne(int colonne) {
+        // on parcourt les lignes de la colonne
+        for (int i = 0; i < Grille.grille.length; i++) {
+            // si la case est vide
+            if (Grille.grille[i][colonne] == 0) {
+                // on joue le coup
+                Grille.grille[i][colonne] = Grille.joueurCourant;
+                break;
+            }
+        }
+    }
+
+    /**
+    * 
+    * @param grille
+    * @param joueur
+    * @return la colonne ou le joueur doit jouer
+    *
+    */
+    public static int meilleurCoup(int[][] grille, int joueur) {
+        int meilleurCoup = 0;
+        int meilleurScore = Integer.MIN_VALUE; // on initialise le meilleur score à -infini
+        int score;
+        for (int i = 0; i < grille[0].length; i++) { // on parcourt les colonnes
+            if (grille[0][i] == 0) { // si la colonne n'est pas pleine
+            score = minimax(grille, i, joueur); // on calcule le score du coup
+            if (score > meilleurScore) { // si le score est meilleur que le meilleur score
+                meilleurScore = score; // on met à jour le meilleur score
+                meilleurCoup = i; // on met à jour le meilleur coup
+            }
+            }
+        }
+        return meilleurCoup;
     }
 
 }
