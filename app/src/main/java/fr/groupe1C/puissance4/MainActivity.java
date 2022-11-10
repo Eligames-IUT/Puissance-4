6fcac29138339d163cdb82730c3fead91f030a44
Julien Charbonnel <juliencharbonnel20@gmail.com>
2022-11-10 14:39:37 +0100

    test

--- app/src/main/java/fr/groupe1C/puissance4/MainActivity.java
+++ app/src/main/java/fr/groupe1C/puissance4/MainActivity.java
@@ -1,4 +1,4 @@
-package fr.groupe1C.puissance4;
+package src.main.java.fr.groupe1C.puissance4;
 
 import android.os.Bundle;
 import android.support.design.widget.Snackbar;
@@ -20,23 +20,24 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
+
 
 public class MainActivity extends AppCompatActivity {
 
     private TextView mGreetingTextView;
     private EditText mNameEditText;
     // on recupere les colonnes avec l'id colonne 1,2,3,4,5,6,7 dans activity_game.xml
-    private Button mColonne1;
-    private Button mColonne2;
-    private Button mColonne3;
-    private Button mColonne4;
-    private Button mColonne5;
-    private Button mColonne6;
-    private Button mColonne7;
-
-    private lateinit var binding: ActivityMainBinding
-
-
+    private Colonne mColonne1;
+    private Colonne mColonne2;
+    private Colonne mColonne3;
+    private Colonne mColonne4;
+    private Colonne mColonne5;
+    private Colonne mColonne6;
+    private Colonne mColonne7;  
+    
 
 
    
@@ -46,29 +47,17 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
 
-        // on récupere les colonnes
-        mColonne1 = findViewById(R.id.colonne1);
-        mColonne2 = findViewById(R.id.colonne2);
-        mColonne3 = findViewById(R.id.colonne3);
-        mColonne4 = findViewById(R.id.colonne4);
-        mColonne5 = findViewById(R.id.colonne5);
-        mColonne6 = findViewById(R.id.colonne6);
-        mColonne7 = findViewById(R.id.colonne7);
+        // on récupere les colonnes dans une file LinkedList<Colonne>
+        Queue<Colonne> colonnes = new LinkedList<Colonne>();
+        for (int i = 1; i <= 7; i++) {
+            int id = getResources().getIdentifier("colonne" + i, "id", getPackageName());
+            Button colonne = findViewById(id);
+            colonnes.add(new Colonne(colonne));
+        }
 
-        // on récupere le textview
-        mGreetingTextView = findViewById(R.id.greeting_text);
-        // on récupere l'edittext
-        mNameEditText = findViewById(R.id.name_edit_text);
-
-        // on ajoute un listener sur les colonnes pour savoir quand on clique dessus
-        mColonne1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // on affiche un message
-                Snackbar.make(v, "Colonne 1", Snackbar.LENGTH_LONG).show();
-            }
-        });
-
-
+        // on affiche les colonnes dans la console
+        for (Colonne colonne : colonnes) {
+            System.out.println(colonne);
+        }
     }
 }
