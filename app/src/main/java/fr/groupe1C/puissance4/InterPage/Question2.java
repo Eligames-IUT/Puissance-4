f4a4ad3cd29962859cfb8c444ef952e60a96ce6b
yassine hamrouni <yassine.hamrouni@etu.u-pec.fr>
2022-11-10 14:51:49 +0100

    Sondage

---
+++ app/src/main/java/fr/groupe1C/puissance4/InterPage/Question2.java
@@ -0,0 +1,15 @@
+package fr.groupe1C.puissance4.InterPage;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import fr.groupe1C.puissance4.R;
+
+public class Question2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        this.setContentView(R.layout.question2);
+    }
+}
---
+++ app/src/main/java/fr/groupe1C/puissance4/Main.java
@@ -0,0 +1,8 @@
+package fr.groupe1C.puissance4;
+import fr.groupe1C.puissance4.InterPage.*;
+
+public class Main {
+    public static void main(String[] args) {
+        new Question1();
+    }
+}
