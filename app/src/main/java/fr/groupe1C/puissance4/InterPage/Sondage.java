3946620cf6b1a2bfa6fca0790f864e871998c929
yassine hamrouni <yassine.hamrouni@etu.u-pec.fr>
2022-11-10 12:34:46 +0100

    Sondage

---
+++ app/src/main/java/fr/groupe1C/puissance4/InterPage/Sondage.java
@@ -0,0 +1,37 @@
+package fr.groupe1C.puissance4.InterPage;
+
+import android.widget.Button;
+import android.view.View;
+
+public class Sondage {
+   private Button oui1;
+   private Button non1;
+
+    private Button oui2;
+    private Button non2;
+
+
+   /** @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game);
+
+        b1 = findViewById(R.id.game_activity_textview_question);
+        mAnswer1 = findViewById(R.id.game_activity_button_1);
+        mAnswer2 = findViewById(R.id.game_activity_button_2);
+        mAnswer3 = findViewById(R.id.game_activity_button_3);
+        mAnswer4 = findViewById(R.id.game_activity_button_4);
+        mRemainingQuestionCount = 4;
+        mScore = 0;
+
+        mAnswer1.setOnClickListener(this);
+        mAnswer2.setOnClickListener(this);
+        mAnswer3.setOnClickListener(this);
+        mAnswer4.setOnClickListener(this);
+
+        mCurrentQuestion = mQuestionBank.getCurrentQuestion();
+        displayQuestion(mCurrentQuestion);
+
+    }**/
+
+}
