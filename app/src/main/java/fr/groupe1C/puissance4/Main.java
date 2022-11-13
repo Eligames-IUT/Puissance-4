02d34fc8b4c1135324303f7ab2807c55887f6257
YAS <yassine.hamrouni@etu.u-pec.fr>
2022-11-13 14:14:33 +0100

    configurer le sdk pour run

--- app/src/main/java/fr/groupe1C/puissance4/Main.java
+++ app/src/main/java/fr/groupe1C/puissance4/Main.java
@@ -1,7 +1,10 @@
 package fr.groupe1C.puissance4;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
 import fr.groupe1C.puissance4.InterPage.*;
 
-public class Main {
+public class Main{
     public static void main(String[] args) {
         new Question1();
     }
