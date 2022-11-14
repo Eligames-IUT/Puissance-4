588b2fee348abdc3da560c59b9e32fe898773b1a
Julien Charbonnel <juliencharbonnel20@gmail.com>
2022-11-14 21:08:25 +0100

    Ajout de Cellules et Jetons

---
+++ app/src/main/java/fr/groupe1C/puissance4/Cellules.java
@@ -0,0 +1,51 @@
+package fr.groupe1C.puissance4;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+/**
+ * @author Julien Charbonnel
+ * @version 1.0
+ * @since 1.0
+ * Classe qui représente une cellule du plateau de jeu
+ */
+public class Cellules {
+
+    private static List<Cellules> cellules = new ArrayList<Cellules>();
+    private int x;
+    private int y;
+    private Jetons content = null;
+    private int id;
+
+    public Cellules(int x, int y) {
+        this.x = x;
+        this.y = y;
+        this.id = spaces.size();
+        cellules.add(this);
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public Token getContent() {
+        return content;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setContent(Jetons content) {
+        this.content = content;
+    }
+
+    public boolean isAvailable(){
+        return this.getContent() == null;
+    }
+}
---
+++ app/src/main/java/fr/groupe1C/puissance4/Jetons.java
@@ -0,0 +1,16 @@
+package fr.groupe1C.puissance4;
+
+
+
+public class Jeton {
+
+    private Joueur player;
+
+    public Jeton(Joueur player) {
+        this.player = player;
+    }
+
+    public Joueur getPlayer() {
+        return player;
+    }
+}
