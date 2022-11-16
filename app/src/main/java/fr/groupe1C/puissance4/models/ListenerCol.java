65c51d0e7cd1327fd6de0b2d59a55774e5f08232
Claire G <claire.gobert2002@gmail.com>
2022-11-16 18:07:22 +0100

    Le click fonctionne

--- app/src/main/java/fr/groupe1C/puissance4/models/ListenerCol.java
+++ app/src/main/java/fr/groupe1C/puissance4/models/ListenerCol.java
@@ -4,10 +4,12 @@ import android.view.View.OnClickListener;
 import android.app.Activity;
 import android.view.View;
 
+import fr.groupe1C.puissance4.R;
+
 public class ListenerCol extends Activity implements OnClickListener {
     
     public void onClick(View v) {
-
+       // v.setBackgroundResource(R.color.jaune);
     }
 
 }
