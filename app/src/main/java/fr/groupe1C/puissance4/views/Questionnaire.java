542c63fa50e9e017f6f5a47a2484fdd7040e4e82
yassine hamrouni <yassine.hamrouni@etu.u-pec.fr>
2022-11-17 14:10:52 +0100

    a faire

--- app/src/main/java/fr/groupe1C/puissance4/views/Questionnaire.java
+++ app/src/main/java/fr/groupe1C/puissance4/views/Questionnaire.java
@@ -3,37 +3,46 @@ package fr.groupe1C.puissance4.views;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.InputType;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+
 import fr.groupe1C.puissance4.R;
 
-public class Questionnaire extends AppCompatActivity implements  View.OnClickListener{
+public class Questionnaire extends AppCompatActivity implements View.OnClickListener {
 
-    private TextView tv;
-
+    private TextView tv1;
+    private TextView tv2;
+    private TextView tv3;
     private String[] question;
-
-    private Button[] oui;
-    private Button[] non;
+    private Object[] reponse;
+    private Button btn1;
+    private Button btn2;
     private Button pas_avis;
 
-    private Button oui2;
-    private Button non2;
 
     private int nb_question;
+    private static int index;
 
 
-   @Override
-   protected void onCreate(Bundle savedInstanceState) {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         this.setContentView(R.layout.questionnaire);
+        Questionnaire.index = 0;
 
+        tv1 = findViewById(R.id.question);
+        tv2 = findViewById(R.id.input);
+        tv3 = findViewById(R.id.combobox);
 
-        tv = findViewById(R.id.question);
+        tv3.setVisibility(View.INVISIBLE);
+        tv2.setVisibility(View.INVISIBLE);
 
-        nb_question = 11;
+        nb_question = 10;
 
         question = new String[nb_question];
 
@@ -43,60 +52,48 @@ public class Questionnaire extends AppCompatActivity implements  View.OnClickLis
         question[3] = "Combien de partie(s) avez-vous joué ?";
         question[4] = "Trouvez-vous le jeu facile d'utilisation ?";
         question[5] = "Quel est votre statut ?";
-        question[6] = "Quel est votre situation patrimoniale ?";
+        question[6] = "Quelle est votre situation matrimoniale ?";
         question[7] = "Quels jeux aimeriez-vous voir prochainement ?";
         question[8] = "Avec qui jouez-vous ?";
         question[9] = "Aimeriez-vous un mode 3 joueurs ?";
 
 
-        oui = new Button[nb_question];
-        non = new Button[nb_question];
-        pas_avis = findViewById(R.id.pas_avis1);
+
+        btn1 = findViewById(R.id.btn1);
+        btn2 = findViewById(R.id.btn2);
+        pas_avis = findViewById(R.id.btn3);
 
 
-        oui[0] = findViewById(R.id.oui);
-        non[0] = findViewById(R.id.non);
-
-        pas_avis = findViewById(R.id.pas_avis1);
-
-        oui[0].setOnClickListener(this);
-        non[0].setOnClickListener(this);
+        btn1.setOnClickListener(this);
+        btn2.setOnClickListener(this);
         pas_avis.setOnClickListener(this);
 
-   }
+        reponse = new Object[nb_question];
+    }
 
     @Override
     public void onClick(View v) {
 
-        if(v.equals(pas_avis)){
-            Intent gameActivityIntent = new Intent(this, MainActivity.class);
-            startActivity(gameActivityIntent);
-        }
-
-            for(int i = 0; i < nb_question; i++){
-
-                if(i == 0){
-                    pas_avis.setVisibility(View.INVISIBLE);
+        switch (Questionnaire.index){
+            case 0:{
+                if(v.equals(this.btn1) || v.equals(this.btn2)){
+                    tv1.setText(this.question[index]);
                 }
+                else{
 
-                if (v.equals(this.oui[i]) || v.equals(this.non[i])){
-
-                    this.oui[i] = null;
-                    this.non[i] = null;
-
-                    this.oui[i+1] = findViewById(R.id.oui);
-                    this.non[i+1] = findViewById(R.id.non);
-
-                    this.oui[i+1].setOnClickListener(this);
-                    this.non[i+1].setOnClickListener(this);
-
-                    tv.setText(question[i]);
-                    break;
                 }
+                Questionnaire.index++;
+                break;
             }
 
+            case 1:{
 
-   }
+                Questionnaire.index++;
+                break;
+            }
+
+            default: break;
+        }
+
+    }
 }
-
-
